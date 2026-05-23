984521c397da2f7d5495f750a6030fd535acff53
liedsonnhapulo-maker <lnhapulo@byupathway.edu>
2026-05-23 23:31:45 +0200

    add scripturememorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,52 @@
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        List<Scripture> scriptures = new List<Scripture>()
+        {
+            new Scripture(
+                new Reference("John", 3, 16),
+                "For God so loved the world that he gave his one and only Son that whoever believes in him shall not perish but have eternal life."
+            ),
+
+            new Scripture(
+                new Reference("Proverbs", 3, 5, 6),
+                "Trust in the Lord with all your heart and lean not on your own understanding in all your ways submit to him and he will make your paths straight."
+            ),
+
+            new Scripture(
+                new Reference("Philippians", 4, 13),
+                "I can do all things through Christ who strengthens me."
+            )
+        };
+
+        Random random = new Random();
+        Scripture selectedScripture = scriptures[random.Next(scriptures.Count)];
+
+        while (!selectedScripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(selectedScripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue or type 'quit' to finish:");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            selectedScripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine(selectedScripture.GetDisplayText());
+        Console.WriteLine("\nProgram ended.");
     }
 }
---
+++ week03/ScriptureMemorizer/scriputure.cs
@@ -0,0 +1,62 @@
+// Scripture.cs
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(' ');
+
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return text;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
